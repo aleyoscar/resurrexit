007878f253d5f97eb60b9916a7cb5871dcfe1dc4
oscarale <mail@aleyoscar.com>
2025-03-12 17:58:25 -0500

    feat(tools): Index parser separates page number and id

--- tools/parse_index.py
+++ tools/parse_index.py
@@ -26,6 +26,7 @@ else:
 		count = 0
 		error = 0
 		line_count = 0
+		ids = []
 		with open(sys.argv[1]) as f:
 			lines = f.readlines()
 		for line in lines:
@@ -33,11 +34,14 @@ else:
 			title = line.strip()
 			title = re.sub(r"\.*", "", title) # Remove periods
 			title = re.sub(r"•*", "", title) # Remove dots
+			title = title.replace('(', '- ') # Replace opening '(' with dashes '-'
+			title = title.replace(')', '') # Remove closing ')'
 			title = ' '.join(title.split()) # Remove duplicate spaces
-			match = re.search(r" (\d+$)", title)
+			match = re.search(r" (\d+)(-\d+)?$", title)
 			if match:
 				try:
 					id = int(match.group(1)) # Set ID
+					page = f"{match.group(1)}{match.group(2)}"
 				except ValueError:
 					print(f"\n<<ERROR>> Cannot convert {match.group(1)} to int on line {line_count}\n")
 					continue
@@ -57,8 +61,10 @@ else:
 						psalm['step'] = ''
 						psalm['tags'] = []
 						psalm['capo'] = 0
+						if lang != 'es-es': psalm['id'] = 0
 					psalm['title'] = title.upper()
-					psalm['id'] = id
+					if lang == 'es-es': psalm['id'] = id
+					psalm['page'] = page
 					psalm['lang'] = lang
 
 					frontmatter.dump(psalm, psalm_file)
