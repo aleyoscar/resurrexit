3bed3f12c3188bc68e01512498c294272e50e2e4
oscarale <mail@aleyoscar.com>
2025-03-21 09:25:03 -0500

    fix(wire): Move admin css changes to admin.css in site/templates

--- processwire/site/templates/admin.php
+++ processwire/site/templates/admin.php
@@ -1,18 +1,21 @@
 <?php namespace ProcessWire;
 
 /**
- * Admin template just loads the admin application controller, 
- * and admin is just an application built on top of ProcessWire. 
+ * Admin template just loads the admin application controller,
+ * and admin is just an application built on top of ProcessWire.
  *
- * This demonstrates how you can use ProcessWire as a front-end 
- * to another application. 
+ * This demonstrates how you can use ProcessWire as a front-end
+ * to another application.
  *
- * Feel free to hook admin-specific functionality from this file, 
+ * Feel free to hook admin-specific functionality from this file,
  * but remember to leave the require() statement below at the end.
- * 
+ *
  * Note: this template file does not use the _init.php or _main.php
- * 
+ *
  */
 
 /** @var Config $config */
-require($config->paths->core . "admin.php"); 
+
+$config->styles->append($config->urls->templates . "styles/admin.css");
+
+require($config->paths->core . "admin.php");
