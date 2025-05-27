b1e06b95c4708b6b57b79440b530520f3fe5c11c
oscarale <mail@aleyoscar.com>
2025-05-27 14:30:47 -0500

    feat(core): Update version info in README and sw

--- gen-chglog.sh
+++ gen-chglog.sh
@@ -7,13 +7,24 @@ else
 	TAG_CONFIG=.chglog/config-tag.yml
 	TAG_FILE=.chglog/current-tag.md
 
+	echo "Updating version info in README.md and sw.js to $1"
+	sed -i '1s/.*/const CACHE_NAME = "resurrexit-$1";/' static/sw.js
+	sed -i '5s/.*/> $1/' README.md
+
+	echo "Generating build files"
+	pipenv shell
+	python freeze.py
+
 	echo "Generating CHANGELOG file"
 	git-chglog --next-tag $1 -o $LOG_FILE
 
 	echo "Generating Tag Message File for $1"
 	git-chglog --config $TAG_CONFIG --next-tag $1 -o $TAG_FILE $1
 
-	echo "Commiting $LOG_FILE and $TAG_FILE"
+	echo "Staging files"
+	git add .
+
+	echo "Commiting"
 	git commit -am "release $1"
 
 	echo "Creating Tag $1"
